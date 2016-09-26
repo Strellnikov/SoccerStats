28bb115ac7dc3026c89e29ca7f88a0b2c6eebd69
strellnikov <brad@bradandtiffany.me>
2016-09-26 19:41:45 -0400

    the remainder of C# Streams & Data Processing

--- Program.cs
+++ Program.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 using Newtonsoft.Json;
-using System.Net;
 
 namespace SoccerStats
 {
@@ -20,17 +19,10 @@ namespace SoccerStats
             fileName = Path.Combine(directory.FullName, "players.json");
             var players = DeserializePlayers(fileName);
             var topTenPlayers = GetTopTenPlayers(players);
-            foreach (var player in topTenPlayers)
+            foreach(var player in topTenPlayers)
             {
-                List<NewsResult> newsResults = GeNewsForPlayer(string.Format("{0} {1}", player.FirstName, player.LastName));
-                foreach(var result in newsResults)
-                {
-                    Console.WriteLine(string.Format("Date: {0:f}, Headline: {1}, Summary: {2} \r\n", result.DatePublished, result.Headline, result.Summary));
-                    Console.ReadKey();
-                }
+                Console.WriteLine("Name: " + player.FirstName + " PPG: " + player.PointsPerGame);
             }
-            fileName = Path.Combine(directory.FullName, "topten.json");
-            SerializePlayerToFile(topTenPlayers, fileName);
         }
 
         public static string ReadFile(string fileName)
@@ -48,7 +40,7 @@ namespace SoccerStats
             {
                 string line = "";
                 reader.ReadLine();
-                while ((line = reader.ReadLine()) != null)
+                while((line = reader.ReadLine()) != null)
                 {
                     var gameResult = new GameResult();
                     string[] values = line.Split(',');
@@ -102,16 +94,15 @@ namespace SoccerStats
             {
                 players = serializer.Deserialize<List<Player>>(jsonReader);
             }
-
+                
             return players;
         }
-
         public static List<Player> GetTopTenPlayers(List<Player> players)
         {
             var topTenPlayers = new List<Player>();
             players.Sort(new PlayerComparer());
             int counter = 0;
-            foreach (var player in players)
+            foreach(var player in players)
             {
                 topTenPlayers.Add(player);
                 counter++;
@@ -120,43 +111,5 @@ namespace SoccerStats
             }
             return topTenPlayers;
         }
-
-        public static void SerializePlayerToFile(List<Player> players, string fileName)
-        {
-            var serializer = new JsonSerializer();
-            using (var writer = new StreamWriter(fileName))
-            using (var jsonWriter = new JsonTextWriter(writer))
-            {
-                serializer.Serialize(jsonWriter, players);
-            }
-        }
-
-        public static string GetGoogleHomePage()
-        {
-            var webClient = new WebClient();
-            byte[] googleHome = webClient.DownloadData("https://www.google.com");
-
-            using (var stream = new MemoryStream(googleHome))
-            using (var reader = new StreamReader(stream))
-            {
-                return reader.ReadToEnd();
-            }
-        }
-
-        public static List<NewsResult> GeNewsForPlayer(string playerName)
-        {
-            var results = new List<NewsResult>();
-            var webClient = new WebClient();
-            webClient.Headers.Add("Ocp-Apim-Subscription-Key", "e9c42cec9a9d4d0a8f5effa95ee2f6b1");
-            byte[] searchResults = webClient.DownloadData(string.Format("https://api.cognitive.microsoft.com/bing/v5.0/news/search?q={0}&mkt=en-us HTTP/1.1", playerName));
-            var serializer = new JsonSerializer();
-            using (var stream = new MemoryStream(searchResults))
-            using (var reader = new StreamReader(stream))
-            using (var jsonReader = new JsonTextReader(reader))
-            {
-                results = serializer.Deserialize<NewsSearch>(jsonReader).NewsResult;
-            }
-            return results;
-
-        }
     }
+}
